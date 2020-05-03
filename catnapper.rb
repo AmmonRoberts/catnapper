7217e5d7ce129de135c789796707ac9f8eca054a
Ammon Roberts <ammoneroberts@gmail.com>
2020-05-03 00:00:46 -0600

    Initial commit

---
+++ catnapper.rb
@@ -0,0 +1,25 @@
+def clear_screen
+    puts "\e[H\e[2J"
+end
+
+
+
+puts("CATNAPPER\n\n")
+input = nil
+while input != "q"
+    puts("Main Menu:\n\nS: Start a new game\n\nL: Load an existing game\n\nQ: Exit Catnapper\n\n")
+    puts("What is your choice?")
+    input=gets.chomp.downcase
+
+    if input == "s"
+        puts("NEW GAME")
+        gets
+        
+    elsif input == "l"
+        puts("LOAD GAME")
+        gets
+        
+    end
+clear_screen
+end
+puts("THANKS FOR PLAYING")
